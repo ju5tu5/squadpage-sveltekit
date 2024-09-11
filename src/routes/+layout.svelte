ef9e8975d3c9a755418554a17817544646e00da1
joostf <j.faber@hva.nl>
2024-09-11 09:50:22 +0200

    Tweaked nav styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,26 @@
 <main>
   <slot></slot>
 </main>
+
+<style>
+  ul {
+   list-style: none;
+   padding:0;
+   display:flex;
+   gap:1rem;
+  }
+ 
+  a {
+   border:1px solid var(--primary);
+     padding:.25rem;
+     border-radius:.25rem;
+     cursor: pointer;
+     transition:.25s;
+     text-decoration: none;
+  }
+  a:hover {
+     translate: -2px -2px;
+     background-color: rebeccapurple;
+     color:#fff;
+   }
+ </style>
